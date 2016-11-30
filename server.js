a699508b8907455c0dbbd4ce84ed736a4b7c48e0
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-11-30 05:22:17 -0800

    Get post route made for user login

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express=require('express');
 var app=express();
 var PORT=process.env.PORT || 3000;
+var users=[];
+var body_parser=require('body-parser');
+
+app.use(body_parser.json());
+//Get all users
+app.get("/user",function(req,res){
+res.send(users);
+});
+
+//Post a user
+app.post("/user",function(req,res){
+var body=req.body;
+console.log(body.password);
+res.send(body);
+});
 
 app.listen(PORT,function(){
 console.log("Listening on PORT :"+PORT);
