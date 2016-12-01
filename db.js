9e3efa5abc833f61e9a8e97069aff573f615dc6c
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-11-30 23:47:49 -0800

    port env added for heroku deployment

--- db.js
+++ db.js
@@ -1,8 +1,19 @@
-var Squeleizer=require('sequelize');
-var sequelize = new Squeleizer(undefined,undefined,undefined,{
+var Sequelize=require('sequelize');
+var env_variable=process.env.NODE_ENV || "development";
+var sequelize;
+if(env_variable==="production")
+{
+sequelize=new Sequelize(process.env.DATABASE_URL,{
+dialect:"postgres"
+});
+}
+else{
+sequelize=new Sequelize(undefined,undefined,undefined,{
 "dialect":"sqlite",
 "storage":__dirname+"/data/user-api.sqlite"
-});
+});	
+}
+
 
 var db={};
 db.user=sequelize.import(__dirname+"/models/users.js");
